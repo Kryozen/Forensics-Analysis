d0623092d9661f08a1f4450745e7c6b89e569dbd
99orazio <63792973+99orazio@users.noreply.github.com>
2021-08-06 11:31:11 +0200

    Update report.js

--- assets/js/report.js
+++ assets/js/report.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
 
     "use strict";
-
-    loadSelectLog();
+    var deviceType = $("#deviceType").val();
+    loadSelectLog(deviceType);
     changeLog();
     changeBtnThresholType();
 
